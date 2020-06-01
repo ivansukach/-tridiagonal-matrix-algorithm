f9ab3cb0490ef914687657678c7d10bec503b8b0
Ivan Sukach <ivan.sukach@effective-soft.com>
2020-06-01 12:44:09 +0300

    first task complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def tri_diagonal_matrix_algorithm(n, b, c, d, e):
@@ -12,5 +13,46 @@ def tri_diagonal_matrix_algorithm(n, b, c, d, e):
     return _x
 
 
-x = tri_diagonal_matrix_algorithm(5, [4, 22, 22, 17, 2], [0, 3, 3, 1, 3], [2, 2, 4, 1, -2], [1, 5, 1, 2, 0])
-print("X=", x)
+def f(_x):
+    return math.sin(_x)*math.sin(_x)
+
+
+def k(_x):
+    return math.cos(_x)*math.cos(_x)+1
+
+
+def k_derivative(_x):
+    return -math.sin(2*_x)
+
+
+x_min = 0
+x_max = 1
+h1 = 0.1
+h2 = 0.01
+n1 = int((x_max-x_min)/h1+1)
+n2 = int((x_max-x_min)/h2+1)
+x_h1 = np.linspace(x_min, x_max, n1)
+x_h2 = np.linspace(x_min, x_max, n2)
+print("Аппроксимация поставленной задачи разностной схемой второго порядка")
+b1_1 = np.zeros(n1)
+c1_1 = np.zeros(n1)
+d1_1 = np.zeros(n1)
+e1_1 = np.zeros(n1)
+
+b1_1[0] = 0
+d1_1[0] = 1
+e1_1[0] = (-2.0)/(h1*h1+h1+2)
+
+b1_1[n1-1] = 1+(h1/2)*f(x_max)+(h1*k_derivative(x_max)/(2*k(x_max)))
+c1_1[n1-1] = -k(x_max)/h1
+d1_1[n1-1] = 1+k(x_max)/h1+h1/2+h1*k_derivative(x_max)/(2*k(x_max))
+
+
+for j in range(1, n1-1):
+    b1_1[j] = -f(x_h1[j])
+    c1_1[j] = -k_derivative(x_h1[j])/(2*h1) + k(x_h1[j])/(h1*h1)
+    d1_1[j] = -2 * k(x_h1[j])/(h1*h1) - 1
+    e1_1[j] = k_derivative(x_h1[j])/(2*h1)+k(x_h1[j])/(h1*h1)
+
+y = tri_diagonal_matrix_algorithm(n1, b1_1, c1_1, d1_1, e1_1)
+print("Y=", y)
