71b0c412bad3f16a35f58d637c69f3948e7aec41
Ivan Sukach <ivan.sukach@effective-soft.com>
2020-06-01 19:48:31 +0300

    stable, h=0.01 added

--- main.py
+++ main.py
@@ -25,127 +25,120 @@ def k_derivative(_x):
     return -math.sin(2*_x)
 
 
+def approx(_h, _n, _x):
+    print("Аппроксимация поставленной задачи разностной схемой второго порядка")
+    b1_1 = np.zeros(_n)
+    c1_1 = np.zeros(_n)
+    d1_1 = np.zeros(_n)
+    e1_1 = np.zeros(_n)
+
+    b1_1[0] = 0
+    d1_1[0] = 1
+    e1_1[0] = (-2.0)/(_h * _h + _h + 2)
+
+    b1_1[_n - 1] = 1 + (_h / 2) * f(x_max) + (_h * k_derivative(x_max) / (2 * k(x_max)))
+    c1_1[_n - 1] = -k(x_max) / _h
+    d1_1[_n - 1] = 1 + k(x_max) / _h + _h / 2 + _h * k_derivative(x_max) / (2 * k(x_max))
+
+    for j in range(1, _n - 1):
+        b1_1[j] = -f(_x[j])
+        c1_1[j] = -k_derivative(_x[j]) / (2 * _h) + k(_x[j]) / (_h * _h)
+        d1_1[j] = -2 * k(_x[j]) / (_h * _h) - 1
+        e1_1[j] = k_derivative(_x[j]) / (2 * _h) + k(_x[j]) / (_h * _h)
+    y1_1 = tri_diagonal_matrix_algorithm(_n, b1_1, c1_1, d1_1, e1_1)
+    print("Решение для h=", _h, " Y=", y1_1)
+
+
+def iim(_h, _n, _x):
+    print("Интегро-интерполяционный метод")
+    a_iim = np.zeros(_n - 1)
+    d_iim = np.zeros(_n)
+    fi_iim = np.zeros(_n)
+    b2_1 = np.zeros(_n)
+    c2_1 = np.zeros(_n)
+    d2_1 = np.zeros(_n)
+    e2_1 = np.zeros(_n)
+
+    d_iim[0] = 1
+    d_iim[_n - 1] = 1
+    fi_iim[0] = f(0.25 * _h)
+    fi_iim[_n - 1] = f(1 - 0.25 * _h)
+
+    for j in range(0, _n - 1):
+        a_iim[j] = k((_x[j]+_x[j+1])/2)
+    for j in range(1, _n - 1):
+        d_iim[j] = 1
+        fi_iim[j] = f(_x[j])
+
+    b2_1[0] = _h * fi_iim[0] / 2
+    d2_1[0] = 1 + _h * d_iim[0] / 2 + a_iim[0] / _h
+    e2_1[0] = - a_iim[0] / _h
+
+    b2_1[_n - 1] = 1 + _h * fi_iim[_n - 1] / 2
+    c2_1[_n - 1] = - a_iim[_n - 2] / _h
+    d2_1[_n - 1] = 1 + _h * d_iim[_n - 1] / 2 + a_iim[_n - 2] / _h
+
+    for j in range(1, _n - 1):
+        b2_1[j] = -fi_iim[j]
+        c2_1[j] = a_iim[j-1]/(_h * _h)
+        d2_1[j] = -(a_iim[j]+a_iim[j-1]) / (_h * _h) - d_iim[j]
+        e2_1[j] = a_iim[j]/(_h * _h)
+
+    y2_1 = tri_diagonal_matrix_algorithm(_n, b2_1, c2_1, d2_1, e2_1)
+    print("Решение для h=", _h, " Y=", y2_1)
+
+
+def vdm(_h, _n, _x):
+    print("Вариационно-разностный метод")
+    a_vdm = np.zeros(_n - 1)
+    d_vdm = np.zeros(_n)
+    fi_vdm = np.zeros(_n)
+    b3_1 = np.zeros(_n)
+    c3_1 = np.zeros(_n)
+    d3_1 = np.zeros(_n)
+    e3_1 = np.zeros(_n)
+
+    d_vdm[0] = 1
+    d_vdm[_n - 1] = 1
+    fi_vdm[0] = f(0)
+    fi_vdm[_n - 1] = f(1)
+
+    for j in range(0, _n - 1):
+        a_vdm[j] = (k(_x[j])+k(_x[j+1]))/2
+    for j in range(1, _n - 1):
+        d_vdm[j] = 1
+        fi_vdm[j] = (f(_x[j])*(_x[j+1]-_x[j])+f(_x[j])*(_x[j]-_x[j-1]))/(2 * _h)
+
+    b3_1[0] = _h * fi_vdm[0] / 2
+    d3_1[0] = 1 + _h * d_vdm[0] / 2 + a_vdm[0] / _h
+    e3_1[0] = - a_vdm[0] / _h
+
+    b3_1[_n - 1] = 1 + _h * fi_vdm[_n - 1] / 2
+    c3_1[_n - 1] = - a_vdm[_n - 2] / _h
+    d3_1[_n - 1] = 1 + _h * d_vdm[_n - 1] / 2 + a_vdm[_n - 2] / _h
+
+    for j in range(1, _n - 1):
+        b3_1[j] = -fi_vdm[j]
+        c3_1[j] = a_vdm[j-1]/(_h * _h)
+        d3_1[j] = -(a_vdm[j]+a_vdm[j-1]) / (_h * _h) - d_vdm[j]
+        e3_1[j] = a_vdm[j]/(_h * _h)
+
+    y3_1 = tri_diagonal_matrix_algorithm(_n, b3_1, c3_1, d3_1, e3_1)
+    print("Решение для h=", _h, " Y=", y3_1)
+
+
 x_min = 0
 x_max = 1
 h1 = 0.1
 h2 = 0.01
-n1 = int((x_max-x_min)/h1+1)
+n1 = int((x_max - x_min) / h1 + 1)
 n2 = int((x_max-x_min)/h2+1)
 x_h1 = np.linspace(x_min, x_max, n1)
 x_h2 = np.linspace(x_min, x_max, n2)
-print("Аппроксимация поставленной задачи разностной схемой второго порядка")
-b1_1 = np.zeros(n1)
-c1_1 = np.zeros(n1)
-d1_1 = np.zeros(n1)
-e1_1 = np.zeros(n1)
+approx(h1, n1, x_h1)
+iim(h1, n1, x_h1)
+vdm(h1, n1, x_h1)
 
-b1_1[0] = 0
-d1_1[0] = 1
-e1_1[0] = (-2.0)/(h1*h1+h1+2)
-
-b1_1[n1-1] = 1+(h1/2)*f(x_max)+(h1*k_derivative(x_max)/(2*k(x_max)))
-c1_1[n1-1] = -k(x_max)/h1
-d1_1[n1-1] = 1+k(x_max)/h1+h1/2+h1*k_derivative(x_max)/(2*k(x_max))
-
-for j in range(1, n1-1):
-    b1_1[j] = -f(x_h1[j])
-    c1_1[j] = -k_derivative(x_h1[j])/(2*h1) + k(x_h1[j])/(h1*h1)
-    d1_1[j] = -2 * k(x_h1[j])/(h1*h1) - 1
-    e1_1[j] = k_derivative(x_h1[j])/(2*h1)+k(x_h1[j])/(h1*h1)
-print("b1_1:", b1_1)
-print("c1_1:", c1_1)
-print("d1_1:", d1_1)
-print("e1_1:", e1_1)
-y1_1 = tri_diagonal_matrix_algorithm(n1, b1_1, c1_1, d1_1, e1_1)
-print("Решение для h=0.1 Y=", y1_1)
-
-
-print("Интегро-интерполяционный метод")
-a_iim = np.zeros(n1-1)
-d_iim = np.zeros(n1)
-fi_iim = np.zeros(n1)
-b2_1 = np.zeros(n1)
-c2_1 = np.zeros(n1)
-d2_1 = np.zeros(n1)
-e2_1 = np.zeros(n1)
-
-d_iim[0] = 1
-d_iim[n1-1] = 1
-fi_iim[0] = f(0.25*h1)
-fi_iim[n1-1] = f(1 - 0.25*h1)
-
-for j in range(0, n1-1):
-    a_iim[j] = k((x_h1[j]+x_h1[j+1])/2)
-for j in range(1, n1 - 1):
-    d_iim[j] = 1
-    fi_iim[j] = f(x_h1[j])
-print("fi_iim", fi_iim)
-print("a_iim", a_iim)
-
-b2_1[0] = h1*fi_iim[0]/2
-d2_1[0] = 1 + h1*d_iim[0]/2 + a_iim[0]/h1
-e2_1[0] = - a_iim[0]/h1
-
-b2_1[n1-1] = 1+h1*fi_iim[n1-1]/2
-c2_1[n1-1] = - a_iim[n1-2]/h1
-d2_1[n1-1] = 1 + h1*d_iim[n1-1]/2 + a_iim[n1-2]/h1
-
-
-for j in range(1, n1-1):
-    b2_1[j] = -fi_iim[j]
-    c2_1[j] = a_iim[j-1]/(h1*h1)
-    d2_1[j] = -(a_iim[j]+a_iim[j-1])/(h1*h1)-d_iim[j]
-    e2_1[j] = a_iim[j]/(h1*h1)
-
-print("b2_1:", b2_1)
-print("c2_1:", c2_1)
-print("d2_1:", d2_1)
-print("e2_1:", e2_1)
-
-y2_1 = tri_diagonal_matrix_algorithm(n1, b2_1, c2_1, d2_1, e2_1)
-print("Решение для h=0.1 Y=", y2_1)
-
-print("Вариационно-разностный метод")
-a_vdm = np.zeros(n1-1)
-d_vdm = np.zeros(n1)
-fi_vdm = np.zeros(n1)
-b3_1 = np.zeros(n1)
-c3_1 = np.zeros(n1)
-d3_1 = np.zeros(n1)
-e3_1 = np.zeros(n1)
-
-d_vdm[0] = 1
-d_vdm[n1-1] = 1
-fi_vdm[0] = f(0)
-fi_vdm[n1-1] = f(1)
-
-for j in range(0, n1-1):
-    a_vdm[j] = (k(x_h1[j])+k(x_h1[j+1]))/2
-for j in range(1, n1 - 1):
-    d_vdm[j] = 1
-    fi_vdm[j] = (f(x_h1[j])*(x_h1[j+1]-x_h1[j])+f(x_h1[j])*(x_h1[j]-x_h1[j-1]))/(2*h1)
-
-print("a_vdm", a_vdm)
-print("d_vdm", d_vdm)
-print("fi_vdm", fi_vdm)
-b3_1[0] = h1*fi_vdm[0]/2
-d3_1[0] = 1 + h1*d_vdm[0]/2 + a_vdm[0]/h1
-e3_1[0] = - a_vdm[0]/h1
-
-b3_1[n1-1] = 1+h1*fi_vdm[n1-1]/2
-c3_1[n1-1] = - a_vdm[n1-2]/h1
-d3_1[n1-1] = 1 + h1*d_vdm[n1-1]/2 + a_vdm[n1-2]/h1
-
-for j in range(1, n1-1):
-    b3_1[j] = -fi_vdm[j]
-    c3_1[j] = a_vdm[j-1]/(h1*h1)
-    d3_1[j] = -(a_vdm[j]+a_vdm[j-1])/(h1*h1)-d_vdm[j]
-    e3_1[j] = a_vdm[j]/(h1*h1)
-
-print("b3_1:", b3_1)
-print("c3_1:", c3_1)
-print("d3_1:", d3_1)
-print("e3_1:", e3_1)
-
-y3_1 = tri_diagonal_matrix_algorithm(n1, b3_1, c3_1, d3_1, e3_1)
-print("Решение для h=0.1 Y=", y3_1)
+approx(h2, n2, x_h2)
+iim(h2, n2, x_h2)
+vdm(h2, n2, x_h2)
